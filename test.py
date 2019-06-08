aebe756e40036aa53f59cfefd2bf7abb08f8f6d7
blacktea-test <51481222+blacktea-test@users.noreply.github.com>
2019-06-08 19:50:27 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print('1');
+var linebot = require('linebot');
+var express = require('express');
+
+var bot = linebot({
+    channelId: '填入自己的資料',
+    channelSecret: '填入自己的資料',
+    channelAccessToken: '填入自己的資料'
+});
+
+bot.on('message', function(event) {
+    event.reply('Hello, 你好');  
+});
