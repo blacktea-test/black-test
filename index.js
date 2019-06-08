91d515cbc2d7b5d177e867918c276ab57260659b
blacktea-test <51481222+blacktea-test@users.noreply.github.com>
2019-06-08 19:59:28 +0800

    Update and rename test.js to index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var linebot = require('linebot');
 var express = require('express');
 
 var bot = linebot({
-    channelId: '露娜test',
+    channelId: '1584016060',
     channelSecret: '7d71d9b52e15b539fa4c3993e44dca9e',
     channelAccessToken: 'cdkwdBVbwTLQyao98X80ZO6zP5+KCcZgcDTLtnopA9oVWK+Mc1ksx67HE5ND8OOvxKbXL75BGl2IG0y/cwD+KGC3arHlRD8DLtE7ICR8UVXnYTtSTttoPyzh7EI5L/zHohxtOFit+Brik63bcyym8gdB04t89/1O/w1cDnyilFU=
 '
